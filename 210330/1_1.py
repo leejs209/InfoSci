46ef9d8591752209c724109553f54469c2004725
Jay Lee <leejs2090209@gmail.com>
2021-03-30 19:54:07 +0900

    Initial commit

---
+++ 210330/1_1.py
@@ -0,0 +1,15 @@
+a = 100
+b = 50
+c = a+b
+d = a/b
+e = a*b
+f = a//b
+g = a%b
+h = a**b
+
+print(c)
+
+# /Users/leejs/PycharmProjects/InfoSci/venv/bin/python /Users/leejs/PycharmProjects/InfoSci/210330/1_1.py
+# 150
+#
+# Process finished with exit code 0
---
+++ 210330/1_2.py
@@ -0,0 +1,12 @@
+name = input('이름?')
+school = input('학교는?')
+grade = input('학년은?')
+print('니는', school+'에 다니는', grade, name+'이구나.')
+
+# /Users/leejs/PycharmProjects/InfoSci/venv/bin/python /Users/leejs/PycharmProjects/InfoSci/210330/1_2.py
+# 이름?이준성
+# 학교는?대륜고
+# 학년은?2학년
+# 니는 대륜고에 다니는 2학년 이준성이구나.
+#
+# Process finished with exit code 0
